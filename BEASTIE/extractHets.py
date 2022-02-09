b82847dc8352db691872b7761b8f448d7c7c7add
Xue (Scarlett) Zou <xz195@duke.edu>
2022-02-09 09:27:59 -0500

    allow using newly filtered gencode annotation

--- BEASTIE/extractHets.py
+++ BEASTIE/extractHets.py
@@ -5,10 +5,10 @@
 
 import logging
 import os
-
+import sys
 import pandas as pd
 from pkg_resources import resource_filename
-
+import gzip
 from BEASTIE.misc_tools.GffTranscriptReader import GffTranscriptReader
 from BEASTIE.misc_tools.Pipe import Pipe
 
@@ -50,39 +50,31 @@ def isHeterozygous(genotype):
 
 
 def count_all_het_sites(
-    sample, vcfFilename, outputFilename, chr_start, chr_end, geneFile
+    sample, vcfFilename, outputFilename, chr_start, chr_end, gencode_path
 ):
     logging.info("..... We are looking at individual: {0}".format(sample))
     filename = os.path.splitext(str(outputFilename))[0]
-    logging.info(
-        "..... start loading gencode annotation file : {0}".format(
-            os.path.basename(geneFile)
-        )
-    )
     count = 0
-    reader = GffTranscriptReader()
-    # geneFile = resource_filename(gencode_path, f"/chr{Num}")
-    all_geneList = reader.loadGenes(geneFile)
-    logging.info(
-        "..... finish loading gencode annotation file {0} with {1} genes".format(
-            geneFile, len(all_geneList)
-        )
-    )
+
     for Num in range(chr_start, chr_end + 1):
+        reader = GffTranscriptReader()
+        geneFile = gencode_path + f"/gencode.chr{Num}.gtf.gz"
+        geneList = reader.loadGenes(geneFile)
         outputFile = filename + ".chr" + str(Num) + ".TEMP.tsv"
         if not os.path.isfile(outputFile):
-            # geneList = filter(
-            #     lambda gene: gene.getSubstrate.strip("chr") == Num, all_geneList
-            # )
-            logging.info("..... Working on chr {0}".format(Num))
-            counter = 0
+            chr = f"chr{Num}"
+            geneList = list(filter(lambda gene: gene.getSubstrate() == chr, geneList))
+            logging.info(
+                "..... start loading gencode annotation chr {0} with {1} genes".format(
+                    Num, len(geneList)
+                )
+            )
             byGene = {}
             total_biSNP = 0
             region_str_to_transcripts = {}
-            for gene in all_geneList:
+            for gene in geneList:
                 # print(gene.getSubstrate().strip("chr"))
                 if str(gene.getSubstrate().strip("chr")) == str(Num):
-                    counter += 1
                     Num_transcript = gene.getNumTranscripts()
                     for n in range(Num_transcript):
                         transcript = gene.getIthTranscript(n)
@@ -180,7 +172,7 @@ def count_all_het_sites(
         else:
             data = pd.read_csv(outputFile, sep="\t", header=0, index_col=False)
             data0 = pd.concat([data0, data])
-        logging.info("..... chr {0} is finished with {1} genes".format(Num, counter))
+
     data0.drop_duplicates()
     data0.to_csv(outputFilename, sep="\t", header=True, index=False)
     for files in os.listdir(os.path.dirname(outputFilename)):
