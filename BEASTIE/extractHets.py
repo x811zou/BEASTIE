122b78597a1cb879cb9fd58ec1353913bf8f889d
x811zou <xuz943@gmail.com>
2022-07-11 11:04:52 -0400

    add the option for ATACseq data

--- BEASTIE/extractHets.py
+++ BEASTIE/extractHets.py
@@ -197,3 +197,68 @@ def count_all_het_sites(
             out_stream.write("\t".join(map(str, r)))
             out_stream.write("\n")
     out_stream.close()
+
+
+def count_all_het_sites_forpeaks(
+    vcfFilename,
+    outputFilename,
+    chr_start,
+    chr_end,
+    annotation_file,
+):
+    out_stream = open(outputFilename, "w")
+    out_stream.write(
+        "chrN\tchr\tpos\tSNP_id\tgenotype\tpeak_start\tpeak_end\tpeakID\tgeneID\n"
+    )
+
+    """
+    1. construct a dict with unique peak region with IDs {peak_region: chr,peak_start_pos,peak_end_pos,geneID, peakID}
+    """
+    peak_region_to_IDs = {}
+    with gzip.open(annotation_file, "rt", newline="\n") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter="\t")
+        for row in reader:
+            key = f"{row['chr']}:{row['peak_start']}-{row['peak_end']}"
+            peak_region_to_IDs[key] = row
+
+    """
+    2. construct a dict with unique peak region with VCF records {peak_region:VCF records}
+    """
+    peak_region_to_snp_infos = tabix_regions(
+        peak_region_to_IDs.keys(), vcfline_processor, vcfFilename
+    )
+
+    """
+    3. write output data
+    """
+    variant_to_peak_info = {}
+    data = []
+    for peak_region in peak_region_to_snp_infos:
+        snp_infos = peak_region_to_snp_infos[peak_region]
+        IDs = peak_region_to_IDs[peak_region]
+        for snp_info in snp_infos:  # loop through each variant record
+            pos, rsid, genotype = snp_info
+            chrId = IDs["chr"]
+            chrom = "chr" + chrId
+            peak_start = IDs["peak_start"]
+            peak_end = IDs["peak_end"]
+            peakID = IDs["peakID"]
+            geneID = IDs["geneID"]
+            data.append(
+                [
+                    chrId,
+                    chrom,
+                    pos,
+                    rsid,
+                    genotype,
+                    peak_start,
+                    peak_end,
+                    peakID,
+                    geneID,
+                ]
+            )
+    data.sort(key=lambda r: (r[1], r[2]))
+    for r in data:
+        out_stream.write("\t".join(map(str, r)))
+        out_stream.write("\n")
+    out_stream.close()
